c0526d2f9947791eb0ceadd5c65e0a9572c2420a
pedroagpacheco <29712227+pedroagpacheco@users.noreply.github.com>
2023-04-21 13:12:45 -0300

    feat: initial commit

---
+++ src/common/components/example/example.tsx
@@ -0,0 +1,13 @@
+import './example.scss'
+
+const Example = () => (
+  <div className="p-4">
+    <h1 className="text-4xl font-bold pb-4">Checklist</h1>
+    <ul>
+      <li>react-router</li>
+      <li>axe + eslint-plugin-jsx-a11y</li>
+    </ul>
+  </div>
+)
+
+export default Example
