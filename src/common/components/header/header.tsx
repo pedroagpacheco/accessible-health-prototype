6f00afe7f80b69a26665f29716b140ba7696deb8
pedroagpacheco <29712227+pedroagpacheco@users.noreply.github.com>
2023-04-23 13:17:48 -0300

    feat: added font size reset option

--- src/common/components/header/header.tsx
+++ src/common/components/header/header.tsx
@@ -1,14 +1,40 @@
-import { faPills,
+import {
+  faPills,
+  faRotate,
   faMagnifyingGlassPlus,
   faMagnifyingGlassMinus
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+enum FontSize {
+  DEFAULT = 16,
+  MAX = 24,
+  MIN = 12,
+  INCREASE = 'plus',
+  DECREASE = 'minus',
+  RESET = 'reset'
+}
+
 const Header = () => {
+  const handleFontSizeChange = (type: FontSize) => {
+    const root = getComputedStyle(document.documentElement)
+    const oldpx = Number.parseInt(root.getPropertyValue('--font-size').replaceAll('px', ''))
+
+    if (type === FontSize.INCREASE) {
+      const canBeIncreased = oldpx + 1 < FontSize.MAX && oldpx + 1 > FontSize.MIN
+      canBeIncreased && document.documentElement.style.setProperty('--font-size', `${oldpx + 1  }px`)
+    } else if (type === FontSize.DECREASE) {
+      const canBeDecreased = oldpx - 1 < FontSize.MAX && oldpx - 1 > FontSize.MIN
+      canBeDecreased && document.documentElement.style.setProperty('--font-size', `${oldpx - 1  }px`)
+    } else {
+      document.documentElement.style.setProperty('--font-size', `${FontSize.DEFAULT  }px`)
+    }
+  }
+
   return (
-    <div className="w-screen h-8 bg-blue-900 flex items-center justify-center">
-      <div className="w-full max-w-5xl flex justify-between">
-        <h1 className="text-xl text-white font-bold">
+    <div className="w-screen py-3 bg-blue-900 flex items-center justify-center">
+      <div className="w-full max-w-5xl flex justify-between text-2xl">
+        <h1 className="text-white font-bold">
           <FontAwesomeIcon icon={faPills} className="pr-2 text-red-400"/>
           ConecteSaúde
         </h1>
@@ -17,22 +43,21 @@ const Header = () => {
             className="text-white hover:text-gray-200 pr-4"
             title="Aumentar fonte"
             aria-label="Aumentar fonte"
-            onClick={() => {
-              const root = getComputedStyle(document.documentElement)
-              const oldpx = Number.parseInt(root.getPropertyValue('--font-size').replaceAll('px', ''))
-              document.documentElement.style.setProperty('--font-size', `${oldpx + 1  }px`)
-            }}>
+            onClick={() => handleFontSizeChange(FontSize.INCREASE)}>
             <FontAwesomeIcon icon={faMagnifyingGlassPlus}/>
+          </button>
+          <button
+            className="text-white hover:text-gray-200 pr-4"
+            title="Redefinir fonte"
+            aria-label="Redefinir fonte"
+            onClick={() => handleFontSizeChange(FontSize.RESET)}>
+            <FontAwesomeIcon icon={faRotate}/>
           </button>
           <button
             className="text-white hover:text-gray-200"
             title="Diminuir fonte"
             aria-label="Diminuir fonte"
-            onClick={() => {
-              const root = getComputedStyle(document.documentElement)
-              const oldpx = Number.parseInt(root.getPropertyValue('--font-size').replaceAll('px', ''))
-              document.documentElement.style.setProperty('--font-size', `${oldpx - 1  }px`)
-            }}>
+            onClick={() => handleFontSizeChange(FontSize.DECREASE)}>
             <FontAwesomeIcon icon={faMagnifyingGlassMinus}/>
           </button>
         </div>
